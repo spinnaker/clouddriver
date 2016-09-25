fed1b5ea70f95552e72306a6cc5b279b6410870e
Travis Tomsu <ttomsu@google.com>
2016-09-25 10:22:29 -0400

    Remove accidental azure change

--- clouddriver-azure/src/main/groovy/com/netflix/spinnaker/clouddriver/azure/resources/common/AzureResourceOpsDescription.groovy
+++ clouddriver-azure/src/main/groovy/com/netflix/spinnaker/clouddriver/azure/resources/common/AzureResourceOpsDescription.groovy
@@ -17,7 +17,6 @@
 package com.netflix.spinnaker.clouddriver.azure.resources.common
 
 import com.netflix.spinnaker.clouddriver.azure.security.AzureCredentials
-import com.netflix.spinnaker.clouddriver.security.resources.CredentialsNameable
 
 class AzureResourceOpsDescription {
   String name
