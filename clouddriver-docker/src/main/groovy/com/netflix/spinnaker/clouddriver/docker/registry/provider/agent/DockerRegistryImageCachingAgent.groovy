2041c414ce62d86cef14351a422d8b884f1f583e
Nick Irvine <nirvine@xmatters.com>
2017-06-22 13:37:02 -0700

    fix hidden exception while pulling docker tags

--- clouddriver-docker/src/main/groovy/com/netflix/spinnaker/clouddriver/docker/registry/provider/agent/DockerRegistryImageCachingAgent.groovy
+++ clouddriver-docker/src/main/groovy/com/netflix/spinnaker/clouddriver/docker/registry/provider/agent/DockerRegistryImageCachingAgent.groovy
@@ -91,7 +91,7 @@ class DockerRegistryImageCachingAgent implements CachingAgent, AccountAware {
       try {
         tags = credentials.client.getTags(repository)
       } catch (e) {
-        log.error("Could not load tags for ${repository}")
+        log.error("Could not load tags for ${repository}", e)
       }
       def name = tags?.name
       def imageTags = tags?.tags
