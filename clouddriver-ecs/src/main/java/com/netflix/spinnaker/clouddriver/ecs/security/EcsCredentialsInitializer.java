07a7f875bc98dc8c64161b36dae57e2c507c3ecd
dkirillov <dkirillov25@gmail.com>
2017-09-19 16:01:10 -0400

    Manual copy, and saving to accountCredentialsRepository.

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/security/EcsCredentialsInitializer.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/security/EcsCredentialsInitializer.java
@@ -1,9 +1,15 @@
 package com.netflix.spinnaker.clouddriver.ecs.security;
 
+import com.netflix.spinnaker.clouddriver.aws.security.AmazonCredentials;
 import com.netflix.spinnaker.clouddriver.aws.security.NetflixAmazonCredentials;
+import com.netflix.spinnaker.clouddriver.aws.security.NetflixAssumeRoleAmazonCredentials;
+import com.netflix.spinnaker.clouddriver.aws.security.config.CredentialsConfig;
+import com.netflix.spinnaker.clouddriver.aws.security.config.CredentialsLoader;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentials;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentialsRepository;
 import com.netflix.spinnaker.clouddriver.security.CredentialsInitializerSynchronizable;
+import com.netflix.spinnaker.fiat.model.Authorization;
+import com.netflix.spinnaker.fiat.model.resources.Permissions;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -26,15 +32,17 @@ public class EcsCredentialsInitializer implements CredentialsInitializerSynchron
 
   @Bean
   @DependsOn("netflixAmazonCredentials")
-  public List<? extends NetflixAmazonCredentials> netflixECSCredentials(ECSCredentialsConfig credentialsConfig,
+  public List<? extends NetflixAmazonCredentials> netflixECSCredentials(CredentialsLoader<? extends NetflixAmazonCredentials> credentialsLoader,
+                                                                        ECSCredentialsConfig credentialsConfig,
                                                                         AccountCredentialsRepository accountCredentialsRepository) throws Throwable {
-    return synchronizeECSAccounts(credentialsConfig, accountCredentialsRepository);
+    return synchronizeECSAccounts(credentialsLoader, credentialsConfig, accountCredentialsRepository);
   }
 
   @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
   @Bean
   @DependsOn("netflixAmazonCredentials")
-  public List<? extends NetflixAmazonCredentials> synchronizeECSAccounts(ECSCredentialsConfig ecsCredentialsConfig,
+  public List<? extends NetflixAmazonCredentials> synchronizeECSAccounts(CredentialsLoader<? extends NetflixAmazonCredentials> credentialsLoader,
+                                                                         ECSCredentialsConfig ecsCredentialsConfig,
                                                                          AccountCredentialsRepository accountCredentialsRepository) throws Throwable {
 
     // TODO: add support for mutable accounts.
@@ -43,13 +51,73 @@ public class EcsCredentialsInitializer implements CredentialsInitializerSynchron
 
     for (AccountCredentials accountCredentials : accountCredentialsRepository.getAll()) {
       if (accountCredentials instanceof NetflixAmazonCredentials) {
-        for (int x = 0; x < ecsCredentialsConfig.getAccounts().size(); x++) {
-          if (ecsCredentialsConfig.getAccounts().get(x).getAwsAccount().equals(accountCredentials.getName())) {
+        for (ECSCredentialsConfig.Account ecsAccount : ecsCredentialsConfig.getAccounts()) {
+          if (ecsAccount.getAwsAccount().equals(accountCredentials.getName())) {
 
-            NetflixAmazonCredentials netflixECSCredentials = (NetflixAmazonCredentials) accountCredentials;
-            netflixECSCredentials.CLOUD_PROVIDER = "ecs";
-            credentials.add(netflixECSCredentials);
-            
+            CredentialsConfig.Account account = new CredentialsConfig.Account();
+            account.setName(ecsAccount.getName());
+            account.setAccountType("ecs");
+            account.setAccountId(accountCredentials.getAccountId());
+            account.setAllowPrivateThirdPartyImages(((NetflixAmazonCredentials) accountCredentials).getAllowPrivateThirdPartyImages());
+            account.setBastionEnabled(((NetflixAmazonCredentials) accountCredentials).getBastionEnabled());
+            account.setBastionHost(((NetflixAmazonCredentials) accountCredentials).getBastionHost());
+            account.setEdda(account.getEdda());
+            accountCredentials.getCredentials();
+            account.setAssumeRole(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getAssumeRole());
+            account.setDiscoveryEnabled(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getDiscoveryEnabled());
+            account.setDiscovery(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getDiscovery());
+            account.setDefaultKeyPair(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getDefaultKeyPair());
+            account.setDefaultSecurityGroups(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getDefaultSecurityGroups());
+            account.setEddaEnabled(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getEddaEnabled());
+            account.setEnvironment(accountCredentials.getEnvironment());
+            account.setFront50(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getFront50());
+            account.setFront50Enabled(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getFront50Enabled());
+            account.setRequiredGroupMembership(accountCredentials.getRequiredGroupMembership());
+            account.setSessionName(((NetflixAssumeRoleAmazonCredentials) accountCredentials).getSessionName());
+
+            List<CredentialsConfig.Region> regions = new LinkedList<>();
+            for (AmazonCredentials.AWSRegion awsRegion : ((NetflixAssumeRoleAmazonCredentials) accountCredentials).getRegions()) {
+              CredentialsConfig.Region region = new CredentialsConfig.Region();
+              region.setAvailabilityZones((List<String>) awsRegion.getAvailabilityZones());
+              region.setDeprecated(awsRegion.getDeprecated());
+              region.setName(awsRegion.getName());
+              region.setPreferredZones((List<String>) awsRegion.getPreferredZones());
+            }
+            account.setRegions(regions);
+
+            List<CredentialsConfig.LifecycleHook> hooks = new LinkedList<>();
+            for (AmazonCredentials.LifecycleHook awsHook : ((NetflixAssumeRoleAmazonCredentials) accountCredentials).getLifecycleHooks()) {
+              CredentialsConfig.LifecycleHook hook = new CredentialsConfig.LifecycleHook();
+              hook.setDefaultResult(awsHook.getDefaultResult());
+              hook.setHeartbeatTimeout(awsHook.getHeartbeatTimeout());
+              hook.setLifecycleTransition(awsHook.getLifecycleTransition());
+              hook.setNotificationTargetARN(awsHook.getNotificationTargetARN());
+              hook.setRoleARN(awsHook.getRoleARN());
+            }
+            account.setLifecycleHooks(hooks);
+
+            Permissions permissions = ((NetflixAssumeRoleAmazonCredentials) accountCredentials).getPermissions();
+            Permissions.Builder permBuilder = new Permissions.Builder();
+            for (String group : permissions.allGroups()) {
+              List<String> roles = new LinkedList<>();
+              roles.add(group);
+              for (Authorization auth : permissions.getAuthorizations(roles)) {
+                permBuilder.add(auth, group);
+              }
+            }
+            account.setPermissions(permBuilder);
+
+            List<CredentialsConfig.Account> accounts = new LinkedList<>();
+            accounts.add(account);
+
+            CredentialsConfig ecsCopy = new CredentialsConfig();
+            ecsCopy.setAccounts(accounts);
+
+            NetflixAmazonCredentials clone = credentialsLoader.load(ecsCopy).get(0);
+            clone.CLOUD_PROVIDER = "ecs";
+            credentials.add(clone);
+
+            accountCredentialsRepository.save(ecsAccount.getName(), clone);
             break;
           }
         }
