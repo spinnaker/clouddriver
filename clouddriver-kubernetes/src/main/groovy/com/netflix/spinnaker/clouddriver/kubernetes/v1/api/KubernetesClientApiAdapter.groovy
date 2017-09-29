c9cb7472204279c9057c929e3b7f7e0423bb24e8
root <prashant.vicky@gmail.com>
2017-09-29 23:53:38 +0530

    fix(provider/kubernetes): handle exception to reduce noise for user(Spelling Mistake)

--- clouddriver-kubernetes/src/main/groovy/com/netflix/spinnaker/clouddriver/kubernetes/v1/api/KubernetesClientApiAdapter.groovy
+++ clouddriver-kubernetes/src/main/groovy/com/netflix/spinnaker/clouddriver/kubernetes/v1/api/KubernetesClientApiAdapter.groovy
@@ -156,7 +156,7 @@ class KubernetesClientApiAdapter {
 
         return list.items
       } catch (ApiException ex) {
-        log.debug("Cannot fetch Statefulsets Required Kubernates VERSION>=1.6")
+        log.debug("Cannot fetch Statefulsets Required Kubernetes VERSION>=1.6")
       }
     }
   }
@@ -176,7 +176,7 @@ class KubernetesClientApiAdapter {
 
         return list.items
       } catch (ApiException ex) {
-        log.debug("Cannot fetch Daemonsets Required Kubernates VERSION>=1.6")
+        log.debug("Cannot fetch Daemonsets Required Kubernetes VERSION>=1.6")
       }
     }
   }
