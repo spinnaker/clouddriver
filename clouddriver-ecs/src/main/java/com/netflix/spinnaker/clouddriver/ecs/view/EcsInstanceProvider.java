6a7a572ae9116f13f66954cc1e3ce4a474c54ffe
dkirillov <dkirillov25@gmail.com>
2018-01-30 12:37:03 -0500

    Corrected variable names.

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/view/EcsInstanceProvider.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/view/EcsInstanceProvider.java
@@ -88,10 +88,10 @@ public class EcsInstanceProvider implements InstanceProvider<EcsTask> {
   }
 
   private boolean isValidId(String id, String region) {
-    String id_regex = "[\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12}";
-    String id_only = String.format("^%s$", id_regex);
-    String arn = String.format("arn:aws:ecs:%s:\\d*:task/%s", region, id_regex);
-    return id.matches(id_only) || id.matches(arn);
+    String idRegex = "[\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12}";
+    String idOnly = String.format("^%s$", idRegex);
+    String arn = String.format("arn:aws:ecs:%s:\\d*:task/%s", region, idRegex);
+    return id.matches(idOnly) || id.matches(arn);
   }
 
 }
