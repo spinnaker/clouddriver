14e579848d714df23236735d88cc93e2750afa7c
dkirillov <dkirillov25@gmail.com>
2018-01-31 16:58:59 -0500

    Copyright year updated.

--- clouddriver-ecs/src/test/groovy/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/DestroyServiceAtomicOperationSpec.groovy
+++ clouddriver-ecs/src/test/groovy/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/DestroyServiceAtomicOperationSpec.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Lookout, Inc.
+ * Copyright 2018 Lookout, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
