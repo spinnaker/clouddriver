6e9fe55a5d33add782e43bd405c1aa8ea8f1aea1
Eric Zimanyi <ezimanyi@google.com>
2019-01-29 12:43:18 -0500

    refactor(google): Move GoogleBatchRequest to google package
    GoogleBatchReqeust is only used by the GCE provider and has GCE-specific
    logic; move it out of the common module so we can keep that module
    containing only logic shared between google providers.

--- clouddriver-google/src/main/groovy/com/netflix/spinnaker/clouddriver/google/provider/agent/GoogleZonalServerGroupCachingAgent.groovy
+++ clouddriver-google/src/main/groovy/com/netflix/spinnaker/clouddriver/google/provider/agent/GoogleZonalServerGroupCachingAgent.groovy
@@ -47,7 +47,7 @@ import com.netflix.spinnaker.clouddriver.google.provider.agent.util.PaginatedReq
 import com.netflix.spinnaker.clouddriver.google.security.AccountForClient
 import com.netflix.spinnaker.clouddriver.google.security.GoogleNamedAccountCredentials
 import com.netflix.spinnaker.clouddriver.google.GoogleExecutor
-import com.netflix.spinnaker.clouddriver.googlecommon.batch.GoogleBatchRequest
+import com.netflix.spinnaker.clouddriver.google.batch.GoogleBatchRequest
 import groovy.transform.Canonical
 import groovy.util.logging.Slf4j
 
