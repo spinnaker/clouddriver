8f4746c9ed66f809800ff2a4d5c46aa4512dda21
Jacob Kiefer <jacobkiefer@google.com>
2019-04-23 16:04:13 -0400

    fix(provider/google): Prevent returning all security groups targeting (#3593)
    service accounts.
    
    GCE firewalls can target network tags or service accounts. We were
    incorrectly returning all firewalls targeting service accounts.

--- clouddriver-google/src/main/groovy/com/netflix/spinnaker/clouddriver/google/provider/view/GoogleClusterProvider.groovy
+++ clouddriver-google/src/main/groovy/com/netflix/spinnaker/clouddriver/google/provider/view/GoogleClusterProvider.groovy
@@ -240,7 +240,7 @@ class GoogleClusterProvider implements ClusterProvider<GoogleCluster.View> {
     }
     serverGroup.loadBalancers = loadBalancers*.view
 
-    serverGroup.securityGroups = GoogleSecurityGroupProvider.getMatchingServerGroupNames(
+    serverGroup.securityGroups = GoogleSecurityGroupProvider.getMatchingSecurityGroupNames(
         account,
         securityGroups,
         serverGroup.instanceTemplateTags,
