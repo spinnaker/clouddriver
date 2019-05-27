49093a817928ee21a18fc3835dbaf0d32a0942a4
neil-yechenwei <v-cheye@microsoft.com>
2019-05-27 10:51:46 +0800

    feat(provider/azure): Enable dns label

--- clouddriver-azure/src/main/groovy/com/netflix/spinnaker/clouddriver/azure/resources/loadbalancer/ops/UpsertAzureLoadBalancerAtomicOperation.groovy
+++ clouddriver-azure/src/main/groovy/com/netflix/spinnaker/clouddriver/azure/resources/loadbalancer/ops/UpsertAzureLoadBalancerAtomicOperation.groovy
@@ -58,6 +58,18 @@ class UpsertAzureLoadBalancerAtomicOperation implements AtomicOperation<Map> {
 
       task.updateStatus(BASE_PHASE, "Beginning load balancer deployment")
 
+      if(description.dnsName) {
+        if(description.dnsName.isBlank()){
+          throw new RuntimeException("Specified dns name $description.dnsName cannot be blank")
+        }
+
+        // Check dns name conflict
+        def isDnsNameAvailable = description.credentials.networkClient.checkDnsNameAvailability(description.dnsName)
+        if (!isDnsNameAvailable) {
+          throw new RuntimeException("Specified dns name $description.dnsName has conflict")
+        }
+      }
+
       description.name = description.loadBalancerName
       def loadBalancerDescription = description.credentials.networkClient.getLoadBalancer(resourceGroupName, description.name)
 
