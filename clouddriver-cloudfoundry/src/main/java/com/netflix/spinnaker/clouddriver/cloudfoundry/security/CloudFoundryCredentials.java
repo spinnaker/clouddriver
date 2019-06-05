97f3481817a757468226ef91975b55d5e4ddc50b
Jammy Louie <jlouie@pivotal.io>
2019-06-05 17:16:35 -0400

    fix(cf): remove sensitive information from credentials payload (#3752)

--- clouddriver-cloudfoundry/src/main/java/com/netflix/spinnaker/clouddriver/cloudfoundry/security/CloudFoundryCredentials.java
+++ clouddriver-cloudfoundry/src/main/java/com/netflix/spinnaker/clouddriver/cloudfoundry/security/CloudFoundryCredentials.java
@@ -36,7 +36,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Getter
-@JsonIgnoreProperties({"credentials", "client"})
+@JsonIgnoreProperties({"credentials", "client", "password"})
 public class CloudFoundryCredentials implements AccountCredentials<CloudFoundryClient> {
 
   private final String name;
