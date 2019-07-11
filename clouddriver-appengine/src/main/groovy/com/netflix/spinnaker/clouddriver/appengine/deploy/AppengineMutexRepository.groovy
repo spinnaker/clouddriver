10efb2f7a2f53af8fb3b35803743981b2af38a5c
Eric Zimanyi <ezimanyi@google.com>
2019-07-10 20:19:12 -0400

    refactor(appengine): Remove unnecessary catch block
    There's no need to catch an exception only to re-throw it in the
    finally block. Replace with an idiomatic try/finally.
    
    Also add some public/private modifiers that will make conversion to
    java in the next commit easier.

--- clouddriver-appengine/src/main/groovy/com/netflix/spinnaker/clouddriver/appengine/deploy/AppengineMutexRepository.groovy
+++ clouddriver-appengine/src/main/groovy/com/netflix/spinnaker/clouddriver/appengine/deploy/AppengineMutexRepository.groovy
@@ -18,45 +18,34 @@ package com.netflix.spinnaker.clouddriver.appengine.deploy
 
 import java.util.concurrent.Semaphore
 
-class AppengineMutexRepository {
-  static HashMap<String, Mutex> mutexRepository = new HashMap<>()
+public class AppengineMutexRepository {
+  private static HashMap<String, Mutex> mutexRepository = new HashMap<>()
 
-  static <T> T atomicWrapper(String mutexKey, Closure<T> doOperation) {
+  public static <T> T atomicWrapper(String mutexKey, Closure<T> doOperation) {
     if (!mutexRepository.containsKey(mutexKey)) {
       mutexRepository.put(mutexKey, new Mutex())
     }
     Mutex mutex = mutexRepository.get(mutexKey)
 
-    // Outside the try {} block, because in the case of an exception being thrown here, we don't want to try unlocking
-    // the mutex in the finally block.
     mutex.lock()
-    T result = null
-    Exception failure
     try {
-      result = doOperation()
-    } catch (Exception e) {
-      failure = e
+      return doOperation()
     } finally {
       mutex.unlock()
-      if (failure) {
-        throw failure
-      } else {
-        return result
-      }
     }
   }
 
-  static class Mutex {
-    Semaphore sem
+  private static class Mutex {
+    private Semaphore sem
 
-    void lock() {
+    public void lock() {
       if (sem == null) {
         sem = new Semaphore(1)
       }
       sem.acquire()
     }
 
-    void unlock() {
+    public void unlock() {
       if (sem == null) {
         throw new IllegalStateException("Attempt made to unlock mutex that was never locked")
       }
@@ -64,4 +53,3 @@ class AppengineMutexRepository {
     }
   }
 }
-
