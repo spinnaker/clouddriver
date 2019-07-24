ea0fafda8a81cb7b8345c516481559a09e9e3936
Rob Zienert <rzienert@netflix.com>
2019-07-24 11:23:42 -0700

    fix(titus): Maybe fix a startup issue, I dont know whats going on

---
+++ clouddriver-titus/src/main/groovy/com/netflix/spinnaker/clouddriver/titus/caching/utils/CachingSchemaUtil.java
@@ -0,0 +1,55 @@
+package com.netflix.spinnaker.clouddriver.titus.caching.utils;
+
+import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider;
+import com.netflix.spinnaker.clouddriver.titus.credentials.NetflixTitusCredentials;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+public class CachingSchemaUtil {
+
+  private final AccountCredentialsProvider accountCredentialsProvider;
+  private final AwsLookupUtil awsLookupUtil;
+
+  private Map<String, CachingSchema> cachingSchemaForAccounts = new LinkedHashMap<>();
+
+  @Autowired
+  public CachingSchemaUtil(
+      AccountCredentialsProvider accountCredentialsProvider, AwsLookupUtil awsLookupUtil) {
+    this.accountCredentialsProvider = accountCredentialsProvider;
+    this.awsLookupUtil = awsLookupUtil;
+  }
+
+  private static CachingSchema cachingSchemaFor(NetflixTitusCredentials credentials) {
+    return credentials.getSplitCachingEnabled() ? CachingSchema.V2 : CachingSchema.V1;
+  }
+
+  public CachingSchema getCachingSchemaForAccount(String account) {
+    return Optional.ofNullable(cachingSchemaForAccounts.get(account)).orElse(CachingSchema.V1);
+  }
+
+  @PostConstruct
+  private void init() {
+    accountCredentialsProvider.getAll().stream()
+        .filter(c -> c instanceof NetflixTitusCredentials)
+        .forEach(
+            c -> {
+              NetflixTitusCredentials credentials = (NetflixTitusCredentials) c;
+              credentials
+                  .getRegions()
+                  .forEach(
+                      region -> {
+                        cachingSchemaForAccounts.put(
+                            credentials.getName(), cachingSchemaFor(credentials));
+                        cachingSchemaForAccounts.put(
+                            awsLookupUtil.awsAccountId(credentials.getName(), region.getName()),
+                            cachingSchemaFor(credentials));
+                      });
+            });
+  }
+}
