fdcebb43a0b5d032ad9bb85bc51f6a02edfc8ba5
Rob Zienert <rzienert@netflix.com>
2019-07-27 13:21:36 -0700

    fix(titus): make bool flag nonnullable

--- clouddriver-titus/src/main/groovy/com/netflix/spinnaker/clouddriver/titus/deploy/handlers/steps/PrepareTitusDeploymentStep.java
+++ clouddriver-titus/src/main/groovy/com/netflix/spinnaker/clouddriver/titus/deploy/handlers/steps/PrepareTitusDeploymentStep.java
@@ -281,7 +281,7 @@ public class PrepareTitusDeploymentStep extends AbstractTitusDeployStep
       description.setImageId(format("%s:%s", sourceJob.getApplicationName(), imageVersion));
     }
 
-    if (description.getSource() != null && description.getSource().getUseSourceCapacity()) {
+    if (description.getSource() != null && description.getSource().isUseSourceCapacity()) {
       description.getCapacity().setMin(sourceJob.getInstancesMin());
       description.getCapacity().setMax(sourceJob.getInstancesMax());
       description.getCapacity().setDesired(sourceJob.getInstancesDesired());
