9e1f6c6bf1688c2cd06a84f9a5870aef9f7f8daa
maggieneterval <mneterval@google.com>
2019-08-06 15:05:29 -0400

    fix(google): add @Nullable annotation to GoogleApplicationProvider

--- clouddriver-google/src/main/groovy/com/netflix/spinnaker/clouddriver/google/provider/view/GoogleApplicationProvider.java
+++ clouddriver-google/src/main/groovy/com/netflix/spinnaker/clouddriver/google/provider/view/GoogleApplicationProvider.java
@@ -39,6 +39,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.annotation.Nullable;
 import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -75,6 +76,7 @@ final class GoogleApplicationProvider implements ApplicationProvider {
     Set<String> instanceIdentifiers;
   }
 
+  @Nullable
   ApplicationCacheData getApplicationCacheData(String name) {
     CacheData cacheData =
         cacheView.get(
