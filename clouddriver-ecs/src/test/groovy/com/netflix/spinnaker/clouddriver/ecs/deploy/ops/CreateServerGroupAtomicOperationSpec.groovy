e2c48d4525afa34ccab865aaff483bd8cda24c1d
spinnakerbot <spinbot@spinnaker.io>
2020-03-02 09:33:33 -0500

    fix(ecs): provide port mappings if legacy target group setting is set (#4340) (#4362)
    Co-authored-by: Clare Liguori <liguori@amazon.com>

--- clouddriver-ecs/src/test/groovy/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/CreateServerGroupAtomicOperationSpec.groovy
+++ clouddriver-ecs/src/test/groovy/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/CreateServerGroupAtomicOperationSpec.groovy
@@ -493,10 +493,10 @@ class CreateServerGroupAtomicOperationSpec extends CommonAtomicOperation {
     description.getApplication() >> 'v1'
     description.getStack() >> 'kcats'
     description.getFreeFormDetails() >> 'liated'
-    description.ecsClusterName = 'test-cluster'
-    description.iamRole = 'None (No IAM role)'
+    description.getEcsClusterName() >> 'test-cluster'
+    description.getIamRole() >> 'None (No IAM role)'
     description.getContainerPort() >> 1337
-    description.targetGroup = 'target-group-arn'
+    description.getTargetGroup() >> 'target-group-arn'
     description.getPortProtocol() >> 'tcp'
     description.getComputeUnits() >> 9001
     description.getReservedMemory() >> 9001
@@ -563,8 +563,8 @@ class CreateServerGroupAtomicOperationSpec extends CommonAtomicOperation {
     description.getApplication() >> 'v1'
     description.getStack() >> 'ecs'
     description.getFreeFormDetails() >> 'test'
-    description.ecsClusterName = 'test-cluster'
-    description.iamRole = 'None (No IAM role)'
+    description.getEcsClusterName() >> 'test-cluster'
+    description.getIamRole() >> 'None (No IAM role)'
     description.getResolvedTaskDefinitionArtifact() >> resolvedArtifact
     description.getContainerToImageMap() >> [
       web: "docker-image-url/one",
@@ -631,8 +631,8 @@ class CreateServerGroupAtomicOperationSpec extends CommonAtomicOperation {
     description.getApplication() >> 'v1'
     description.getStack() >> 'ecs'
     description.getFreeFormDetails() >> 'test'
-    description.ecsClusterName = 'test-cluster'
-    description.iamRole = 'None (No IAM role)'
+    description.getEcsClusterName() >> 'test-cluster'
+    description.getIamRole() >> 'None (No IAM role)'
     description.getLaunchType() >> 'FARGATE'
     description.getResolvedTaskDefinitionArtifact() >> resolvedArtifact
     description.getContainerToImageMap() >> [
@@ -678,7 +678,7 @@ class CreateServerGroupAtomicOperationSpec extends CommonAtomicOperation {
     description.getApplication() >> 'v1'
     description.getStack() >> 'ecs'
     description.getFreeFormDetails() >> 'test'
-    description.ecsClusterName = 'test-cluster'
+    description.getEcsClusterName() >> 'test-cluster'
     description.getLaunchType() >> 'FARGATE'
     description.getNetworkMode() >> 'awsvpc'
     description.getResolvedTaskDefinitionArtifact() >> resolvedArtifact
@@ -729,6 +729,7 @@ class CreateServerGroupAtomicOperationSpec extends CommonAtomicOperation {
   def 'should use same port for host and container in host mode'() {
     given:
     def description = Mock(CreateServerGroupDescription)
+    description.getTargetGroup() >> 'target-group-arn'
     description.getContainerPort() >> 10000
     description.getNetworkMode() >> 'host'
     def operation = new CreateServerGroupAtomicOperation(description)
