798ee6be53cce990aa20c13a58c2fb7fed47b416
Eric Zimanyi <ezimanyi@google.com>
2020-08-14 16:41:37 -0400

    refactor(kubernetes): Clean up filterArtifacts function
    Rename the verbose filterKubernetesArtifactsByNamespaceAndAccount
    to simply filterArtifacts.
    
    To make the logic more clear split it out into a few predicates
    and use those predicates in the filter logic.
    
    Finally, require namespace to be non-null and push that requirement
    outward. In one case there's something we don't know for sure is
    non-null so convert it with nullToEmpty.

--- clouddriver-kubernetes/src/main/java/com/netflix/spinnaker/clouddriver/kubernetes/artifact/ArtifactReplacer.java
+++ clouddriver-kubernetes/src/main/java/com/netflix/spinnaker/clouddriver/kubernetes/artifact/ArtifactReplacer.java
@@ -35,6 +35,7 @@ import com.netflix.spinnaker.kork.artifacts.model.Artifact;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 import lombok.Value;
@@ -56,53 +57,38 @@ public class ArtifactReplacer {
     this.replacers = ImmutableList.copyOf(replacers);
   }
 
-  private static ImmutableList<Artifact> filterKubernetesArtifactsByNamespaceAndAccount(
-      String namespace, String account, List<Artifact> artifacts) {
+  private static ImmutableList<Artifact> filterArtifacts(
+      @Nonnull String namespace, @Nonnull String account, List<Artifact> artifacts) {
     return artifacts.stream()
-        // Keep artifacts that either aren't k8s, or are in the same namespace and account as our
-        // manifest
-        .filter(
-            a -> {
-              String type = a.getType();
-              if (Strings.isNullOrEmpty(type)) {
-                log.warn("Artifact {} without a type, ignoring", a);
-                return false;
-              }
-
-              if (!type.startsWith("kubernetes/")) {
-                return true;
-              }
-
-              boolean locationMatches;
-              String location = a.getLocation();
-              if (Strings.isNullOrEmpty(location)) {
-                locationMatches = Strings.isNullOrEmpty(namespace);
-              } else {
-                locationMatches = location.equals(namespace);
-              }
-
-              boolean accountMatches;
-              String artifactAccount = getAccount(a);
-              // If the artifact fails to provide an account, we'll assume this was unintentional
-              // and match anyways
-              accountMatches =
-                  Strings.isNullOrEmpty(artifactAccount) || artifactAccount.equals(account);
-
-              return accountMatches && locationMatches;
-            })
+        .filter(a -> !Strings.isNullOrEmpty(a.getType()))
+        .filter(nonKubernetes().or(namespaceMatches(namespace).and(accountMatches(account))))
         .collect(toImmutableList());
   }
 
-  private static String getAccount(Artifact artifact) {
-    return Strings.nullToEmpty((String) artifact.getMetadata("account"));
+  private static Predicate<Artifact> nonKubernetes() {
+    return a -> !a.getType().startsWith("kubernetes/");
+  }
+
+  private static Predicate<Artifact> namespaceMatches(@Nonnull String namespace) {
+    return a -> Strings.nullToEmpty(a.getLocation()).equals(namespace);
+  }
+
+  private static Predicate<Artifact> accountMatches(@Nonnull String account) {
+    return a -> {
+      String artifactAccount = Strings.nullToEmpty((String) a.getMetadata("account"));
+      // If the artifact fails to provide an account, assume this was unintentional and match
+      // anyways
+      return artifactAccount.isEmpty() || artifactAccount.equals(account);
+    };
   }
 
   @Nonnull
   public ReplaceResult replaceAll(
-      KubernetesManifest input, List<Artifact> artifacts, String namespace, String account) {
+      KubernetesManifest input,
+      List<Artifact> artifacts,
+      @Nonnull String namespace,
+      @Nonnull String account) {
     log.debug("Doing replacement on {} using {}", input, artifacts);
-    ImmutableList<Artifact> filteredArtifacts =
-        filterKubernetesArtifactsByNamespaceAndAccount(namespace, account, artifacts);
     DocumentContext document;
     try {
       document = JsonPath.using(configuration).parse(mapper.writeValueAsString(input));
@@ -111,6 +97,7 @@ public class ArtifactReplacer {
       throw new RuntimeException(e);
     }
 
+    ImmutableList<Artifact> filteredArtifacts = filterArtifacts(namespace, account, artifacts);
     ImmutableSet.Builder<Artifact> replacedArtifacts = new ImmutableSet.Builder<>();
     replacers.forEach(
         replacer ->
