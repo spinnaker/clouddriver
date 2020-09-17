57da015a2153eb6376494f370128d0eb2be3c46c
Eric Zimanyi <ezimanyi@google.com>
2020-09-17 12:27:38 -0400

    refactor(kubernetes): Remove KubernetesOnDemandCachingAgent
    This agent no longer adds any value; classes that extend it can
    just directly extend KubernetesCachingAgent.

--- clouddriver-kubernetes/src/main/java/com/netflix/spinnaker/clouddriver/kubernetes/caching/agent/KubernetesCoreCachingAgent.java
+++ clouddriver-kubernetes/src/main/java/com/netflix/spinnaker/clouddriver/kubernetes/caching/agent/KubernetesCoreCachingAgent.java
@@ -30,7 +30,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class KubernetesCoreCachingAgent extends KubernetesOnDemandCachingAgent {
+public class KubernetesCoreCachingAgent extends KubernetesCachingAgent {
   public KubernetesCoreCachingAgent(
       KubernetesNamedAccountCredentials namedAccountCredentials,
       ObjectMapper objectMapper,
