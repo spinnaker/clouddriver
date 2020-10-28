9ce248399fd7e4c18c86d07761227de9f2a7fec3
Manabu Mccloskey <manabu.mccloskey@gmail.com>
2020-10-28 16:35:56 -0600

    feat(ecs): Implement kork credentials to allow for dynamic account loading

--- clouddriver-ecs/src/integration/java/com/netflix/spinnaker/clouddriver/ecs/test/CreateServerGroupWithArtifactsSpec.java
+++ clouddriver-ecs/src/integration/java/com/netflix/spinnaker/clouddriver/ecs/test/CreateServerGroupWithArtifactsSpec.java
@@ -140,8 +140,6 @@ public class CreateServerGroupWithArtifactsSpec extends EcsSpec {
         generateStringFromTestFile("/createServerGroup-artifact-EC2-targetGroupMappings.json");
     String expectedServerGroupName = "ecs-integArtifactsEC2TgMappingsStack-detailTest-v000";
 
-    setEcsAccountCreds();
-
     ByteArrayInputStream byteArrayInputStreamOfArtifactsForEC2Type =
         new ByteArrayInputStream(
             generateStringFromTestArtifactFile(
@@ -237,8 +235,6 @@ public class CreateServerGroupWithArtifactsSpec extends EcsSpec {
     String requestBody =
         generateStringFromTestFile("/createServerGroup-artifact-FARGATE-targetGroupMappings.json");
     String expectedServerGroupName = "ecs-integArtifactsFargateTgMappingsStack-detailTest-v000";
-
-    setEcsAccountCreds();
 
     ByteArrayInputStream byteArrayInputStreamOfArtifactsForFargateType =
         new ByteArrayInputStream(
