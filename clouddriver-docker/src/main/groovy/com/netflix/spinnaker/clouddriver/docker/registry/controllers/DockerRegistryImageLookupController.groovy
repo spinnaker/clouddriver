b02b45e1487d426e9945c139f2216147035c8a89
KeisukeYamashita <19yamashita15@gmail.com>
2020-11-14 19:24:46 +0900

    fix(docker): remove no need import

--- clouddriver-docker/src/main/groovy/com/netflix/spinnaker/clouddriver/docker/registry/controllers/DockerRegistryImageLookupController.groovy
+++ clouddriver-docker/src/main/groovy/com/netflix/spinnaker/clouddriver/docker/registry/controllers/DockerRegistryImageLookupController.groovy
@@ -29,8 +29,6 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-import java.time.Instant
-
 @RestController
 @RequestMapping("/dockerRegistry/images")
 class DockerRegistryImageLookupController {
