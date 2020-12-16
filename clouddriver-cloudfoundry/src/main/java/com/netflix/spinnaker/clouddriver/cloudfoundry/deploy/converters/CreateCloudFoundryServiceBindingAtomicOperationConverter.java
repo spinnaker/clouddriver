75328ce07d469093ea9d7014573b373407eb4835
zach smith <zachdsmith2015@gmail.com>
2020-12-16 13:40:52 -0800

    feat(cloudfoundry): add service-binding stage

---
+++ clouddriver-cloudfoundry/src/main/java/com/netflix/spinnaker/clouddriver/cloudfoundry/deploy/converters/CreateCloudFoundryServiceBindingAtomicOperationConverter.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.netflix.spinnaker.clouddriver.cloudfoundry.deploy.converters;
+
+import com.netflix.spinnaker.clouddriver.cloudfoundry.CloudFoundryOperation;
+import com.netflix.spinnaker.clouddriver.cloudfoundry.client.CloudFoundryApiException;
+import com.netflix.spinnaker.clouddriver.cloudfoundry.deploy.description.CreateCloudFoundryServiceBindingDescription;
+import com.netflix.spinnaker.clouddriver.cloudfoundry.deploy.ops.CreateCloudFoundryServiceBindingAtomicOperation;
+import com.netflix.spinnaker.clouddriver.helpers.OperationPoller;
+import com.netflix.spinnaker.clouddriver.orchestration.AtomicOperation;
+import com.netflix.spinnaker.clouddriver.orchestration.AtomicOperations;
+import java.util.Map;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@CloudFoundryOperation(AtomicOperations.CREATE_SERVICE_BINDINGS)
+@Component
+public class CreateCloudFoundryServiceBindingAtomicOperationConverter
+    extends AbstractCloudFoundryServerGroupAtomicOperationConverter {
+
+  private final OperationPoller operationPoller;
+
+  public CreateCloudFoundryServiceBindingAtomicOperationConverter(
+      @Qualifier("cloudFoundryOperationPoller") OperationPoller operationPoller) {
+    this.operationPoller = operationPoller;
+  }
+
+  @Nullable
+  @Override
+  public AtomicOperation convertOperation(Map input) {
+    return new CreateCloudFoundryServiceBindingAtomicOperation(
+        operationPoller, convertDescription(input));
+  }
+
+  @Override
+  public CreateCloudFoundryServiceBindingDescription convertDescription(Map input) {
+    CreateCloudFoundryServiceBindingDescription description =
+        getObjectMapper().convertValue(input, CreateCloudFoundryServiceBindingDescription.class);
+    description.setCredentials(getCredentialsObject(input.get("credentials").toString()));
+    description.setClient(getClient(input));
+    description.setServerGroupId(
+        getServerGroupId(
+            description.getServerGroupName(), description.getRegion(), description.getClient()));
+    findSpace(description.getRegion(), description.getClient())
+        .ifPresentOrElse(
+            description::setSpace,
+            () -> {
+              throw new CloudFoundryApiException("Could not determine CloudFoundry Space.");
+            });
+
+    return description;
+  }
+}
