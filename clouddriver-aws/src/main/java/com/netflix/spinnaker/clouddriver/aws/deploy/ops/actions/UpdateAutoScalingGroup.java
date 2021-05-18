626effd6769b6db0756ef8fe944f7db85abe5294
Prathibha Datta-Kumar <3614196+pdk27@users.noreply.github.com>
2021-05-17 20:05:13 -0500

    feat(aws): Adding support for modifying mixed instances policy properties and converting a ASG using launch template to use mixed instances policy
      New features in modifyServerGroupLaunchTemplate operation:
      - convert a server group backed by launch template to use mixed instances policy
      - added a new action `PrepareUpdateAutoScalingGroup` to modify operation
      - added unit tests for modify op actions
      - ability to skip modifying launch template (creating a new version) when not required
    
      Changes to existing features:
      - new validation in modify op: modify a launch template only if launch template config changes are requested
      - ModifyServerGroupLaunchTemplateDescription: remove association with launch configuration code path
      - refactors and typo fix
      - API call for creating a new launch template version: New version is created without using source version
    
      https://github.com/spinnaker/spinnaker/issues/5989

--- clouddriver-aws/src/main/java/com/netflix/spinnaker/clouddriver/aws/deploy/ops/actions/UpdateAutoScalingGroup.java
+++ clouddriver-aws/src/main/java/com/netflix/spinnaker/clouddriver/aws/deploy/ops/actions/UpdateAutoScalingGroup.java
@@ -17,27 +17,40 @@
 
 package com.netflix.spinnaker.clouddriver.aws.deploy.ops.actions;
 
+import com.amazonaws.services.autoscaling.model.AutoScalingGroup;
+import com.amazonaws.services.autoscaling.model.InstancesDistribution;
+import com.amazonaws.services.autoscaling.model.LaunchTemplate;
+import com.amazonaws.services.autoscaling.model.LaunchTemplateOverrides;
 import com.amazonaws.services.autoscaling.model.LaunchTemplateSpecification;
+import com.amazonaws.services.autoscaling.model.MixedInstancesPolicy;
 import com.amazonaws.services.autoscaling.model.UpdateAutoScalingGroupRequest;
+import com.amazonaws.services.ec2.model.LaunchTemplateVersion;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import com.netflix.spinnaker.clouddriver.aws.deploy.description.ModifyServerGroupLaunchTemplateDescription;
 import com.netflix.spinnaker.clouddriver.aws.deploy.ops.ModifyServerGroupLaunchTemplateAtomicOperation.LaunchTemplateException;
 import com.netflix.spinnaker.clouddriver.aws.security.NetflixAmazonCredentials;
+import com.netflix.spinnaker.clouddriver.aws.services.LaunchTemplateService;
 import com.netflix.spinnaker.clouddriver.aws.services.RegionScopedProviderFactory;
 import com.netflix.spinnaker.clouddriver.event.EventMetadata;
 import com.netflix.spinnaker.clouddriver.saga.SagaCommand;
 import com.netflix.spinnaker.clouddriver.saga.flow.SagaAction;
 import com.netflix.spinnaker.clouddriver.saga.models.Saga;
 import com.netflix.spinnaker.credentials.CredentialsRepository;
+import java.util.List;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import lombok.Builder;
 import lombok.Value;
 import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
+/** Action to update an AWS EC2 Auto Scaling Group. */
+@Slf4j
 @Component
 public class UpdateAutoScalingGroup
     implements SagaAction<UpdateAutoScalingGroup.UpdateAutoScalingGroupCommand> {
@@ -60,19 +73,150 @@ public class UpdateAutoScalingGroup
         (NetflixAmazonCredentials) credentialsRepository.getOne(description.getAccount());
     RegionScopedProviderFactory.RegionScopedProvider regionScopedProvider =
         regionScopedProviderFactory.forRegion(credentials, description.getRegion());
-    try {
-      regionScopedProvider
-          .getAutoScaling()
-          .updateAutoScalingGroup(
-              new UpdateAutoScalingGroupRequest()
-                  .withAutoScalingGroupName(description.getAsgName())
-                  .withLaunchTemplate(command.launchTemplateSpecification));
-    } catch (Exception e) {
-      throw new LaunchTemplateException(
-          String.format("Failed to update server group %s", description.getAsgName()), e);
+
+    saga.log("[SAGA_ACTION] Updating EC2 Auto Scaling Group " + description.getAsgName());
+
+    // build update request
+    UpdateAutoScalingGroupRequest updateReq =
+        new UpdateAutoScalingGroupRequest().withAutoScalingGroupName(description.getAsgName());
+
+    AutoScalingGroup autoScalingGroup =
+        getAutoScalingGroup(description.getAsgName(), regionScopedProvider);
+    boolean isAsgBackedByMip = autoScalingGroup.getMixedInstancesPolicy() != null;
+
+    if (isAsgBackedByMip) {
+      // update existing MIP
+      final MixedInstancesPolicy existingMipInAsg = autoScalingGroup.getMixedInstancesPolicy();
+
+      MixedInstancesPolicy mip =
+          new MixedInstancesPolicy()
+              .withLaunchTemplate(
+                  new LaunchTemplate()
+                      .withLaunchTemplateSpecification(
+                          new LaunchTemplateSpecification()
+                              .withLaunchTemplateId(
+                                  command.launchTemplateVersion.getLaunchTemplateId())
+                              .withVersion(
+                                  String.valueOf(command.launchTemplateVersion.getVersionNumber())))
+                      .withOverrides(
+                          command.launchTemplateOverrides != null
+                              ? command.launchTemplateOverrides
+                              : existingMipInAsg.getLaunchTemplate().getOverrides()))
+              .withInstancesDistribution(
+                  new InstancesDistribution()
+                      .withOnDemandAllocationStrategy(
+                          description.getOnDemandAllocationStrategy() != null
+                              ? description.getOnDemandAllocationStrategy()
+                              : existingMipInAsg
+                                  .getInstancesDistribution()
+                                  .getOnDemandAllocationStrategy())
+                      .withOnDemandBaseCapacity(
+                          description.getOnDemandBaseCapacity() != null
+                              ? description.getOnDemandBaseCapacity()
+                              : existingMipInAsg
+                                  .getInstancesDistribution()
+                                  .getOnDemandBaseCapacity())
+                      .withOnDemandPercentageAboveBaseCapacity(
+                          description.getOnDemandPercentageAboveBaseCapacity() != null
+                              ? description.getOnDemandPercentageAboveBaseCapacity()
+                              : existingMipInAsg
+                                  .getInstancesDistribution()
+                                  .getOnDemandPercentageAboveBaseCapacity())
+                      .withSpotAllocationStrategy(
+                          description.getSpotAllocationStrategy() != null
+                              ? description.getSpotAllocationStrategy()
+                              : existingMipInAsg
+                                  .getInstancesDistribution()
+                                  .getSpotAllocationStrategy())
+                      .withSpotInstancePools(
+                          description.getSpotInstancePools() != null
+                              ? description.getSpotInstancePools()
+                              : existingMipInAsg.getInstancesDistribution().getSpotInstancePools())
+                      .withSpotMaxPrice(
+                          StringUtils.isNotBlank(description.getSpotPrice())
+                              ? description.getSpotPrice()
+                              : existingMipInAsg.getInstancesDistribution().getSpotMaxPrice()));
+
+      updateReq.withMixedInstancesPolicy(mip);
+    } else {
+      if (command.isReqToUpgradeAsgToMixedInstancesPolicy) {
+        // create new MIP
+        MixedInstancesPolicy mip =
+            new MixedInstancesPolicy()
+                .withLaunchTemplate(
+                    new LaunchTemplate()
+                        .withLaunchTemplateSpecification(
+                            new LaunchTemplateSpecification()
+                                .withLaunchTemplateId(
+                                    command.launchTemplateVersion.getLaunchTemplateId())
+                                .withVersion(
+                                    String.valueOf(
+                                        command.launchTemplateVersion.getVersionNumber())))
+                        .withOverrides(command.launchTemplateOverrides))
+                .withInstancesDistribution(
+                    new InstancesDistribution()
+                        .withOnDemandAllocationStrategy(description.getOnDemandAllocationStrategy())
+                        .withOnDemandBaseCapacity(description.getOnDemandBaseCapacity())
+                        .withOnDemandPercentageAboveBaseCapacity(
+                            description.getOnDemandPercentageAboveBaseCapacity())
+                        .withSpotAllocationStrategy(description.getSpotAllocationStrategy())
+                        .withSpotInstancePools(description.getSpotInstancePools())
+                        .withSpotMaxPrice(description.getSpotPrice()));
+
+        updateReq.withMixedInstancesPolicy(mip);
+      } else {
+        updateReq.withLaunchTemplate(
+            new LaunchTemplateSpecification()
+                .withLaunchTemplateId(command.launchTemplateVersion.getLaunchTemplateId())
+                .withVersion(String.valueOf(command.launchTemplateVersion.getVersionNumber())));
+      }
     }
 
+    try {
+      regionScopedProvider.getAutoScaling().updateAutoScalingGroup(updateReq);
+    } catch (Exception e) {
+      StringBuilder exceptionMsg =
+          new StringBuilder("Failed to update server group " + description.getAsgName() + ".");
+      if (StringUtils.isNotBlank(e.getMessage())) {
+        exceptionMsg.append(" Error message(s): " + e.getMessage());
+      }
+
+      try {
+        // Clean up newly created launch template version by the Saga Flow, in order to keep the
+        // latest version unaltered.
+        // This step is required because only the default and latest launch template versions for a
+        // launch template are cached.
+        // Not cleaning up will result in Internal Server Error for Clouddriver API requests and
+        // subsequent Deck errors.
+        if (command.getNewLaunchTemplateVersionNumber() != null) {
+          log.info("Cleaning up to keep the operation atomic.");
+          cleanUpOnFailure(
+              regionScopedProvider.getLaunchTemplateService(),
+              command.getLaunchTemplateVersion().getLaunchTemplateId(),
+              command.getNewLaunchTemplateVersionNumber());
+        }
+      } catch (Exception ex) {
+        exceptionMsg.append(ex.getMessage());
+      }
+      throw new LaunchTemplateException(exceptionMsg.toString(), e);
+    }
     return new Result();
+  }
+
+  private AutoScalingGroup getAutoScalingGroup(
+      String autoScalingGroupName,
+      RegionScopedProviderFactory.RegionScopedProvider regionScopedProvider) {
+    try {
+      return regionScopedProvider.getAsgService().getAutoScalingGroup(autoScalingGroupName);
+    } catch (Exception e) {
+      throw new LaunchTemplateException(
+          String.format("Failed to get server group %s.", autoScalingGroupName), e);
+    }
+  }
+
+  private void cleanUpOnFailure(
+      LaunchTemplateService ltService, String launchTemplateId, Long ltVersionToDelete) {
+    ltService.deleteLaunchTemplateVersion(launchTemplateId, ltVersionToDelete);
   }
 
   @Builder(builderClassName = "UpdateAutoScalingGroupCommandBuilder", toBuilder = true)
@@ -81,14 +225,16 @@ public class UpdateAutoScalingGroup
   @JsonTypeName("updateAutoScalingGroupCommand")
   @Value
   public static class UpdateAutoScalingGroupCommand implements SagaCommand {
-    @Nonnull private LaunchTemplateSpecification launchTemplateSpecification;
     @Nonnull private ModifyServerGroupLaunchTemplateDescription description;
-
+    @Nonnull private LaunchTemplateVersion launchTemplateVersion;
+    @Nullable private Long newLaunchTemplateVersionNumber;
+    @Nullable private List<LaunchTemplateOverrides> launchTemplateOverrides;
+    @Nonnull private Boolean isReqToUpgradeAsgToMixedInstancesPolicy;
     @NonFinal private EventMetadata metadata;
 
     @Override
-    public void setMetadata(@NotNull EventMetadata eventMetadata) {
-      this.metadata = eventMetadata;
+    public void setMetadata(@NotNull EventMetadata metadata) {
+      this.metadata = metadata;
     }
 
     @JsonPOJOBuilder(withPrefix = "")
