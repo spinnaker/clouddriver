8988fa08aae05cad721e63e1f1d91e260bb36e41
Zach Smith <33258732+zachsmith1@users.noreply.github.com>
2021-08-02 11:15:24 -0600

    feat(lambda): run sdk calls concurrently (#5452)
    Co-authored-by: Raúl Vázquez <raul.vazquez@armory.io>
    (cherry picked from commit ba0ce6986d3e9416866a6e55efb10de54cb80fd6)
    
    # Conflicts:
    #	clouddriver-lambda/clouddriver-lambda.gradle
    #	clouddriver-lambda/src/main/java/com/netflix/spinnaker/clouddriver/lambda/provider/agent/LambdaCachingAgent.java
    #	clouddriver-lambda/src/test/java/com/netflix/spinnaker/clouddriver/lambda/provider/agent/LambdaCachingAgentTest.java

--- clouddriver-lambda/src/main/java/com/netflix/spinnaker/clouddriver/lambda/provider/agent/LambdaCachingAgent.java
+++ clouddriver-lambda/src/main/java/com/netflix/spinnaker/clouddriver/lambda/provider/agent/LambdaCachingAgent.java
@@ -20,10 +20,7 @@ import static com.netflix.spinnaker.cats.agent.AgentDataType.Authority.AUTHORITA
 import static com.netflix.spinnaker.clouddriver.core.provider.agent.Namespace.APPLICATIONS;
 import static com.netflix.spinnaker.clouddriver.lambda.cache.Keys.Namespace.LAMBDA_FUNCTIONS;
 
-import com.amazonaws.auth.policy.*;
-import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.model.*;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.frigga.Names;
 import com.netflix.spectator.api.DefaultRegistry;
@@ -37,47 +34,54 @@ import com.netflix.spinnaker.cats.cache.CacheData;
 import com.netflix.spinnaker.cats.cache.DefaultCacheData;
 import com.netflix.spinnaker.cats.provider.ProviderCache;
 import com.netflix.spinnaker.clouddriver.aws.AmazonCloudProvider;
-import com.netflix.spinnaker.clouddriver.aws.data.ArnUtils;
 import com.netflix.spinnaker.clouddriver.aws.provider.AwsProvider;
 import com.netflix.spinnaker.clouddriver.aws.security.AmazonClientProvider;
 import com.netflix.spinnaker.clouddriver.aws.security.NetflixAmazonCredentials;
 import com.netflix.spinnaker.clouddriver.cache.OnDemandAgent;
 import com.netflix.spinnaker.clouddriver.cache.OnDemandMetricsSupport;
 import com.netflix.spinnaker.clouddriver.cache.OnDemandType;
+import com.netflix.spinnaker.clouddriver.core.limits.ServiceLimitConfiguration;
 import com.netflix.spinnaker.clouddriver.lambda.cache.Keys;
+<<<<<<< HEAD
+=======
+import com.netflix.spinnaker.clouddriver.lambda.service.LambdaService;
+import com.netflix.spinnaker.clouddriver.lambda.service.config.LambdaServiceConfig;
+import com.netflix.spinnaker.kork.exceptions.SpinnakerException;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+>>>>>>> ba0ce6986 (feat(lambda): run sdk calls concurrently (#5452))
 import java.util.*;
-import java.util.function.Predicate;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LambdaCachingAgent implements CachingAgent, AccountAware, OnDemandAgent {
-  private static final TypeReference<Map<String, Object>> ATTRIBUTES =
-      new TypeReference<Map<String, Object>>() {};
-
   private static final Set<AgentDataType> types =
-      new HashSet<AgentDataType>() {
+      new HashSet<>() {
         {
           add(AUTHORITATIVE.forType(LAMBDA_FUNCTIONS.ns));
         }
       };
 
-  private final ObjectMapper objectMapper;
-
-  private final AmazonClientProvider amazonClientProvider;
   private final NetflixAmazonCredentials account;
   private final String region;
   private OnDemandMetricsSupport metricsSupport;
   private final Registry registry;
+<<<<<<< HEAD
+=======
+  private final Clock clock = Clock.systemDefaultZone();
+  private LambdaService lambdaService;
+>>>>>>> ba0ce6986 (feat(lambda): run sdk calls concurrently (#5452))
 
   LambdaCachingAgent(
       ObjectMapper objectMapper,
       AmazonClientProvider amazonClientProvider,
       NetflixAmazonCredentials account,
-      String region) {
-    this.objectMapper = objectMapper;
-
-    this.amazonClientProvider = amazonClientProvider;
+      String region,
+      LambdaServiceConfig lambdaServiceConfig,
+      ServiceLimitConfiguration serviceLimitConfiguration) {
     this.account = account;
     this.region = region;
     this.registry = new DefaultRegistry();
@@ -86,6 +90,14 @@ public class LambdaCachingAgent implements CachingAgent, AccountAware, OnDemandA
             registry,
             this,
             AmazonCloudProvider.ID + ":" + AmazonCloudProvider.ID + ":" + OnDemandType.Function);
+    this.lambdaService =
+        new LambdaService(
+            amazonClientProvider,
+            account,
+            region,
+            objectMapper,
+            lambdaServiceConfig,
+            serviceLimitConfiguration);
   }
 
   @Override
@@ -112,8 +124,10 @@ public class LambdaCachingAgent implements CachingAgent, AccountAware, OnDemandA
   public CacheResult loadData(ProviderCache providerCache) {
     log.info("Describing items in {}", getAgentType());
 
-    AWSLambda lambda = amazonClientProvider.getAmazonLambda(account, region);
+    Map<String, CacheData> lambdaCacheData = new ConcurrentHashMap<>();
+    Map<String, Collection<String>> appLambdaRelationships = new ConcurrentHashMap<>();
 
+<<<<<<< HEAD
     String nextMarker = null;
     List<FunctionConfiguration> lstFunction = new ArrayList<FunctionConfiguration>();
 
@@ -260,6 +274,145 @@ public class LambdaCachingAgent implements CachingAgent, AccountAware, OnDemandA
     } while (nextMarker != null && nextMarker.length() != 0);
 
     return eventSourceMappingConfigurations;
+=======
+    // Get All Lambda's
+    List<Map<String, Object>> allLambdas;
+    try {
+      allLambdas = lambdaService.getAllFunctions();
+    } catch (Exception e) {
+      throw new SpinnakerException(
+          "Failed to populate the lambda cache for account '"
+              + account.getName()
+              + "' and region '"
+              + region
+              + "' because: "
+              + e.getMessage());
+    }
+
+    buildCacheData(lambdaCacheData, appLambdaRelationships, allLambdas);
+
+    Collection<CacheData> processedOnDemandCache = new ArrayList<>();
+
+    // Process on demand cache
+    Collection<CacheData> onDemandCacheData =
+        providerCache
+            .getAll(
+                ON_DEMAND.getNs(),
+                providerCache.filterIdentifiers(
+                    ON_DEMAND.getNs(),
+                    Keys.getLambdaFunctionKey(getAccountName(), getRegion(), "*")))
+            .stream()
+            .filter(d -> (int) d.getAttributes().get("processedCount") == 0)
+            .collect(Collectors.toList());
+
+    for (CacheData onDemandItem : onDemandCacheData) {
+      try {
+        long cachedAt = (long) onDemandItem.getAttributes().get("cacheTime");
+        if (cachedAt > loadDataStart) {
+          CacheData currentLambda = lambdaCacheData.get(onDemandItem.getId());
+          if (currentLambda != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+            LocalDateTime onDemandLastModified =
+                LocalDateTime.parse(
+                    (String) onDemandItem.getAttributes().get("lastModified"), formatter);
+            LocalDateTime currentLambdaLastModified =
+                LocalDateTime.parse(
+                    (String) currentLambda.getAttributes().get("lastModified"), formatter);
+            if (onDemandLastModified.isAfter(currentLambdaLastModified)) {
+              lambdaCacheData.put(onDemandItem.getId(), onDemandItem);
+              String appKey =
+                  onDemandItem.getRelationships().get(APPLICATIONS.ns).stream().findFirst().get();
+              Collection<String> functionkeys =
+                  appLambdaRelationships.getOrDefault(appKey, new ArrayList<>());
+              functionkeys.add(onDemandItem.getId());
+              appLambdaRelationships.put(appKey, functionkeys);
+            }
+          } else {
+            lambdaCacheData.put(onDemandItem.getId(), onDemandItem);
+            String appKey =
+                onDemandItem.getRelationships().get(APPLICATIONS.ns).stream().findFirst().get();
+            Collection<String> functionkeys =
+                appLambdaRelationships.getOrDefault(appKey, new ArrayList<>());
+            functionkeys.add(onDemandItem.getId());
+            appLambdaRelationships.put(appKey, functionkeys);
+          }
+        }
+        Map<String, Object> attr = onDemandItem.getAttributes();
+        attr.put("processedCount", 1);
+        processedOnDemandCache.add(
+            new DefaultCacheData(onDemandItem.getId(), attr, Collections.emptyMap()));
+      } catch (Exception e) {
+        log.warn("Failed to process onDemandCache for Lambda's: " + e.getMessage());
+      }
+    }
+
+    // Create the INFORMATIVE spinnaker application cache with lambda relationships
+    Collection<CacheData> appCacheData = new LinkedList<>();
+    for (String appKey : appLambdaRelationships.keySet()) {
+      appCacheData.add(
+          new DefaultCacheData(
+              appKey,
+              Collections.emptyMap(),
+              Collections.singletonMap(LAMBDA_FUNCTIONS.ns, appLambdaRelationships.get(appKey))));
+    }
+
+    Map<String, Collection<CacheData>> cacheResults = new HashMap<>();
+
+    cacheResults.put(LAMBDA_FUNCTIONS.ns, lambdaCacheData.values());
+    cacheResults.put(APPLICATIONS.ns, appCacheData);
+    cacheResults.put(ON_DEMAND.ns, processedOnDemandCache);
+
+    Map<String, Collection<String>> evictions =
+        computeEvictableData(lambdaCacheData.values(), providerCache);
+
+    log.info("Caching {} items in {}", String.valueOf(lambdaCacheData.size()), getAgentType());
+    return new DefaultCacheResult(cacheResults, evictions);
+  }
+
+  private void buildCacheData(
+      Map<String, CacheData> lambdaCacheData,
+      Map<String, Collection<String>> appLambdaRelationships,
+      List<Map<String, Object>> allLambdas) {
+    allLambdas.parallelStream()
+        .forEach(
+            lf -> {
+              String functionName = (String) lf.get("functionName");
+              String functionKey =
+                  Keys.getLambdaFunctionKey(getAccountName(), getRegion(), functionName);
+
+              /* TODO: If the functionName follows frigga by chance (i.e. somename-someothername), it will try to store the
+                 lambda as a relationship with the app name (somename), even if it wasn't deployed by spinnaker!
+              */
+              // Add the spinnaker application relationship and store it
+              Names names = Names.parseName(functionName);
+              if (names.getApp() != null) {
+                String appKey =
+                    com.netflix.spinnaker.clouddriver.aws.data.Keys.getApplicationKey(
+                        names.getApp());
+                appLambdaRelationships.compute(
+                    appKey,
+                    (k, v) -> {
+                      Collection<String> fKeys = v;
+                      if (fKeys == null) fKeys = new ArrayList<>();
+                      fKeys.add(functionKey);
+                      return fKeys;
+                    });
+                // No other thread should be putting the same function in this map. Its safe to use
+                // put
+                lambdaCacheData.put(
+                    functionKey,
+                    new DefaultCacheData(
+                        functionKey,
+                        lf,
+                        Collections.singletonMap(
+                            APPLICATIONS.ns, Collections.singletonList(appKey))));
+              } else {
+                // TODO: Do we care about non spinnaker deployed lambdas?
+                lambdaCacheData.put(
+                    functionKey, new DefaultCacheData(functionKey, lf, Collections.emptyMap()));
+              }
+            });
+>>>>>>> ba0ce6986 (feat(lambda): run sdk calls concurrently (#5452))
   }
 
   private final Map<String, Object> addConfigAttributes(
@@ -299,6 +452,7 @@ public class LambdaCachingAgent implements CachingAgent, AccountAware, OnDemandA
 
     String appKey = com.netflix.spinnaker.clouddriver.aws.data.Keys.getApplicationKey(appName);
 
+<<<<<<< HEAD
     AWSLambda lambda = amazonClientProvider.getAmazonLambda(account, region);
 
     GetFunctionResult functionResult = null;
@@ -311,10 +465,34 @@ public class LambdaCachingAgent implements CachingAgent, AccountAware, OnDemandA
     Map<String, Object> attributes = new HashMap<String, Object>();
     attributes.put("name", appName);
     Map<String, Collection<String>> evictions = Collections.emptyMap();
+=======
+    Map<String, Object> lambdaAttributes = null;
+    try {
+      lambdaAttributes = lambdaService.getFunctionByName(functionName);
+    } catch (Exception e) {
+      if (e instanceof ResourceNotFoundException) {
+        // do nothing, the lambda was deleted
+      } else {
+        throw new SpinnakerException(
+            "Failed to populate the onDemandCache for lambda '" + functionName + "'");
+      }
+    }
+>>>>>>> ba0ce6986 (feat(lambda): run sdk calls concurrently (#5452))
 
     Collection<String> existingFunctionRel = null;
 
+<<<<<<< HEAD
     CacheData application = providerCache.get(APPLICATIONS.ns, appKey);
+=======
+    if (lambdaAttributes != null && !lambdaAttributes.isEmpty()) {
+      lambdaAttributes.put("cacheTime", clock.instant().toEpochMilli());
+      lambdaAttributes.put("processedCount", 0);
+      DefaultCacheData lambdaCacheData =
+          new DefaultCacheData(
+              functionKey,
+              lambdaAttributes,
+              Collections.singletonMap(APPLICATIONS.ns, Collections.singletonList(appKey)));
+>>>>>>> ba0ce6986 (feat(lambda): run sdk calls concurrently (#5452))
 
     if (null != application && null != application.getRelationships()) {
       existingFunctionRel = application.getRelationships().get(LAMBDA_FUNCTIONS.ns);
@@ -378,6 +556,7 @@ public class LambdaCachingAgent implements CachingAgent, AccountAware, OnDemandA
     }
   }
 
+<<<<<<< HEAD
   private List<String> getTargetGroupNames(AWSLambda lambda, String functionName) {
     List<String> targetGroupNames = new ArrayList<>();
     Predicate<Statement> isAllowStatement =
@@ -417,5 +596,66 @@ public class LambdaCachingAgent implements CachingAgent, AccountAware, OnDemandA
     }
 
     return targetGroupNames;
+=======
+  /**
+   * Provides the key namespace that the caching agent is authoritative of. Currently only supports
+   * the caching agent being authoritative over one key namespace. Taken from
+   * AbstractEcsCachingAgent
+   *
+   * @return Key namespace.
+   */
+  String getAuthoritativeKeyName() {
+    Collection<AgentDataType> authoritativeNamespaces =
+        getProvidedDataTypes().stream()
+            .filter(agentDataType -> agentDataType.getAuthority().equals(AUTHORITATIVE))
+            .collect(Collectors.toSet());
+
+    if (authoritativeNamespaces.size() != 1) {
+      throw new RuntimeException(
+          "LambdaCachingAgent supports only one authoritative key namespace. "
+              + authoritativeNamespaces.size()
+              + " authoritative key namespace were given.");
+    }
+
+    return authoritativeNamespaces.iterator().next().getTypeName();
+  }
+
+  Map<String, Collection<String>> computeEvictableData(
+      Collection<CacheData> newData, ProviderCache providerCache) {
+
+    // Get all old keys from the cache for the region and account
+    String authoritativeKeyName = getAuthoritativeKeyName();
+    Set<String> oldKeys =
+        providerCache.getIdentifiers(authoritativeKeyName).stream()
+            .filter(
+                key -> {
+                  Map<String, String> keyParts = Keys.parse(key);
+                  return keyParts.get("account").equalsIgnoreCase(account.getName())
+                      && keyParts.get("region").equalsIgnoreCase(region);
+                })
+            .collect(Collectors.toSet());
+
+    // New data can only come from the current account and region, no need to filter.
+    Set<String> newKeys = newData.stream().map(CacheData::getId).collect(Collectors.toSet());
+
+    Set<String> evictedKeys =
+        oldKeys.stream().filter(oldKey -> !newKeys.contains(oldKey)).collect(Collectors.toSet());
+
+    Map<String, Collection<String>> evictionsByKey = new HashMap<>();
+    evictionsByKey.put(getAuthoritativeKeyName(), evictedKeys);
+    String prettyKeyName =
+        CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, getAuthoritativeKeyName());
+
+    log.info(
+        "Evicting "
+            + evictedKeys.size()
+            + " "
+            + prettyKeyName
+            + (evictedKeys.size() > 1 ? "s" : "")
+            + " in "
+            + getAgentType());
+
+    return evictionsByKey;
+>>>>>>> ba0ce6986 (feat(lambda): run sdk calls concurrently (#5452))
   }
 }
