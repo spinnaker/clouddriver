d0455acb0d38930d09dbabf618bbfa927eb8a606
Matt Sicker <msicker@apple.com>
2022-01-07 17:08:09 -0600

    Use correct annotations for rest APIs

--- clouddriver-web/src/main/java/com/netflix/spinnaker/clouddriver/controllers/CredentialsController.java
+++ clouddriver-web/src/main/java/com/netflix/spinnaker/clouddriver/controllers/CredentialsController.java
@@ -41,10 +41,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/credentials")
+@RestController
+@RequestMapping("/credentials")
 public class CredentialsController {
   private final AccountDefinitionRepository repository;
   private final CredentialsConfiguration credentialsConfiguration;
