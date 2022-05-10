fff9a00cb4a5c4735594184a4cbfae2502c94cca
U-CORP\j.tandi <j.tandi@j-tandi01.ea.corp.samsungelectronics.net>
2022-05-10 11:40:02 +0800

    fix(clouddriver-aws) Unable to pull ec2 instance list in AWS China region

--- clouddriver-aws/src/main/java/com/netflix/spinnaker/clouddriver/aws/provider/agent/AmazonInstanceTypeCachingAgent.java
+++ clouddriver-aws/src/main/java/com/netflix/spinnaker/clouddriver/aws/provider/agent/AmazonInstanceTypeCachingAgent.java
@@ -86,12 +86,16 @@ public class AmazonInstanceTypeCachingAgent implements CachingAgent {
     this.credentialsRepository = credentialsRepository;
     if (region.startsWith("cn-")) {
       pricingHost = HttpHost.create("https://pricing.cn-north-1.amazonaws.com.cn");
+      pricingUri = URI.create("/offers/v1.0/aws/AmazonEC2/current/index.json");
+      this.httpClient = httpClient;
     } else {
       pricingHost = HttpHost.create("https://pricing.us-east-1.amazonaws.com");
+      pricingUri = URI.create("/offers/v1.0/aws/AmazonEC2/current/" + region + "/index.json");
+      this.httpClient = httpClient;
+
     }
-    pricingUri = URI.create("/offers/v1.0/aws/AmazonEC2/current/" + region + "/index.json");
-    this.httpClient = httpClient;
   }
+
 
   @Override
   public Collection<AgentDataType> getProvidedDataTypes() {
