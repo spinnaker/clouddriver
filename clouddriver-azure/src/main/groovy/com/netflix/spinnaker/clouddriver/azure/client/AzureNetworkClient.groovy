684f92479a7f806e37cdeff396c2bffe06e4b3de
Kevin McCarpenter™️ <kevin@moderne.io>
2022-06-01 15:42:53 -0400

    fix(provider/azure): Remove unused serverGroups tags

--- clouddriver-azure/src/main/groovy/com/netflix/spinnaker/clouddriver/azure/client/AzureNetworkClient.groovy
+++ clouddriver-azure/src/main/groovy/com/netflix/spinnaker/clouddriver/azure/client/AzureNetworkClient.groovy
@@ -304,7 +304,6 @@ class AzureNetworkClient extends AzureBaseClient {
 
         appGateway.update()
           .withTag("cluster", parsedName.cluster)
-          .withTag("serverGroups", agDescription.serverGroups.join(" "))
           .defineBackend(serverGroupName)
           .attach()
           .apply()
@@ -347,14 +346,6 @@ class AzureNetworkClient extends AzureBaseClient {
           chain = chain.withoutTag("cluster")
         }
 
-        // TODO: debug only; remove this as part of the cleanup
-        agDescription.serverGroups?.remove(serverGroupName)
-        if (!agDescription.serverGroups || agDescription.serverGroups.isEmpty()) {
-          chain = chain.withoutTag("serverGroups")
-        } else {
-          chain = chain.withTag("serverGroups", agDescription.serverGroups.join(" "))
-        }
-
         chain.apply()
       }
 
@@ -380,10 +371,7 @@ class AzureNetworkClient extends AzureBaseClient {
     if(loadBalancer) {
       // the application gateway must have an backend address pool list (even if it might be empty)
       if (!loadBalancer.backends()?.containsKey(serverGroupName)) {
-        String sgTag = loadBalancer.tags().get("serverGroups")
-        sgTag = sgTag == null || sgTag.length() == 0 ? serverGroupName : sgTag + " " + serverGroupName
         loadBalancer.update()
-          .withTag("serverGroups", sgTag)
           .defineBackend(serverGroupName)
           .attach()
           .apply()
@@ -416,21 +404,6 @@ class AzureNetworkClient extends AzureBaseClient {
       if (lbAP) {
         def chain = loadBalancer.update()
           .withoutBackend(lbAP.name())
-
-        String sgTag = loadBalancer.tags().get("serverGroups")
-        String[] tags = sgTag.split(" ")
-        String newTag = ""
-        for(String tag: tags) {
-          if(!tag.equals(serverGroupName)) {
-            newTag = newTag.length() == 0 ? tag : newTag + " " + tag
-          }
-        }
-
-        if (newTag.length() == 0) {
-          chain = chain.withoutTag("serverGroups")
-        } else {
-          chain = chain.withTag("serverGroups", newTag)
-        }
 
         chain.apply()
       }
@@ -1167,5 +1140,4 @@ class AzureNetworkClient extends AzureBaseClient {
   String getProviderNamespace() {
     "Microsoft.Network"
   }
-
 }
