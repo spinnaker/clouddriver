654b36a6a73793179bc8ad786ce38a3cf9bb6b56
Matt Gogerly <matt.gogerly@jpmchase.com>
2023-04-05 13:06:36 +0100

    fix(metrics): remove unused extension

--- clouddriver-core/src/test/java/com/netflix/spinnaker/clouddriver/cache/MetricInstrumentationTest.java
+++ clouddriver-core/src/test/java/com/netflix/spinnaker/clouddriver/cache/MetricInstrumentationTest.java
@@ -32,10 +32,7 @@ import java.util.Collection;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class MetricInstrumentationTest {
 
   private Registry registry;
