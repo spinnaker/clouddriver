3f8379adfe9fda6c5a0fe2c6a9fffbfc0f63ca8c
Kirill Batalin <kirill.batalin@transferwise.com>
2024-04-11 21:26:41 +0100

    fix(account): Make docker account properties comparable, thus reduce load on DB and docker registry
    With enabled dynamic account management for docker accounts, `BasicCredentialsLoader` regularly loads accounts from a storage, filters changed accounts and updates them in `MapBackedCredentialsRepository`. Then, for all updated accounts `CredentialsLifecycleHandler` reschedules caching agents (basically, just deletes the agents and adds them again).
    
    Docker accounts settings class doesn't implement `equals` and `hashCode` so even identical settings are considered as different, and Clouddriver reschedules caching agents each polling.
    This leads to increased polling frequency and polling interval isn't respected, because recreated caching agents are ready to run right after scheduling.
    
    The final load on the DB depends on a number of Clouddriver pods, docker accounts and dynamic accounts polling interval. In our case the docker images table is top by reads, with this fix we decreased the reads by 20% and 50% (`cats_v1_taggedImage` and `cats_v1_imageId` tables respectively).
    
    I didn't implement `equals` and `hashCode` for other `CredentialsDefinition`'s child classes because I couldn't test other classes in a real environment. But I'm happy to implement the methods for all of them if you're okay with this.

--- clouddriver-docker/src/main/groovy/com/netflix/spinnaker/clouddriver/docker/registry/config/DockerRegistryConfigurationProperties.groovy
+++ clouddriver-docker/src/main/groovy/com/netflix/spinnaker/clouddriver/docker/registry/config/DockerRegistryConfigurationProperties.groovy
@@ -18,13 +18,16 @@ package com.netflix.spinnaker.clouddriver.docker.registry.config
 
 import com.fasterxml.jackson.annotation.JsonTypeName
 import com.netflix.spinnaker.credentials.definition.CredentialsDefinition
+import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
 @ToString(includeNames = true)
+@EqualsAndHashCode
 class DockerRegistryConfigurationProperties {
 
   @ToString(includeNames = true)
   @JsonTypeName("dockerRegistry")
+  @EqualsAndHashCode
   static class ManagedAccount implements CredentialsDefinition {
     String name
     String environment
